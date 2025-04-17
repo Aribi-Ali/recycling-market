7f15b9267a1c0d366f3c0abae54bf776e4301939
Mohamed <mohamedbelabbes1832@gmail.com>
2025-04-17 22:43:12 +0100

    implemented method that make the notification as read

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AddressController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CKEditorController;
 use App\Http\Controllers\LocationController;
+use App\Http\Controllers\NotificationController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductApprovalController;
 use App\Http\Controllers\ProductController;
@@ -41,6 +42,8 @@ Route::group(
             Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
             Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
             Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+            Route::post('/notifications/read/{id}', [NotificationController::class, 'markAsRead'])->name('notifications.read');
 
             // Address
             Route::get("/addresses", [AddressController::class, "index"])->name("addresses.index");
