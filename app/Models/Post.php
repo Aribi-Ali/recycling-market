598e857b80d9da61e00d071b5f8deaff08126af1
Aribi Ali Aek <aribialilo7@gmail.com>
2025-05-05 17:48:26 +0100

    WIP: local changes before switching branches

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,18 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
 class Post extends Model
 {
-    protected $fillable = ['title', 'slug', 'content', 'featured_image', 'published_at'];
-
-    public static function boot()
-    {
-        parent::boot();
-
-        static::creating(function ($post) {
-            $post->slug = Str::slug($post->title);
-        });
-    }
+    //
 }
